36533ea9193061271f4ad8208217686cf5900f77
Mario Arana <marioknaus22@gmail.com>
2025-02-01 19:04:36 +0100

    create initial home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,32 @@
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
+import profilePic from "../assets/images/clown.jpg"; // Replace with actual image path
+
 export default function Home() {
-    return (
-      <div className="container mt-4">
-        <h1>Home Page</h1>
-        <p>Welcome to the home page!</p>
-      </div>
-    );
-  }
-  
+  return (
+    <Container className="mt-5">
+      <Row className="align-items-center">
+        {/* Left Column: Profile Image */}
+        <Col md={6} className="text-center text-md-start">
+          <Image
+            src={profilePic}
+            alt="Clown"
+            roundedCircle
+            fluid
+            width="400"
+            height="400"
+          />
+        </Col>
+
+        {/* Right Column: Text */}
+        <Col md={6}>
+          <h1>Hello, I'm Clown</h1>
+          <p>
+            Welcome to my portfolio. I am a lead software engineer and founder of Google.
+            I'm passionate about building amazing things with technology.
+          </p>
+          <Button variant="primary">Learn More</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
