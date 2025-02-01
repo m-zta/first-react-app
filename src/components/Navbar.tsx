21f02a6f0372f333835f720702bdb1013a3e4e8f
Mario Arana <marioknaus22@gmail.com>
2025-02-01 16:47:46 +0100

    improve navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,60 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from "react-router-dom"; // ✅ Directly import Link
+// Documentation on react-bootstrap: https://react-bootstrap.netlify.app/
+// Documentation on react-router components: https://v5.reactrouter.com/web/guides/primary-components
+
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"; // to create the navbar
+import { Link } from "react-router-dom"; // to create links in the navbar to different pages of the app
+import { Form, FormControl, Button } from "react-bootstrap"; // to create a search bar in the navbar
 
 export default function MyNavbar() {
   return (
-    <Navbar expand="lg" bg="dark" variant="dark">
+    <Navbar expand="md" bg="primary" data-bs-theme="dark">
+      {/* data-bs-theme is the new way. variant is deprecated */}
       <Container>
-        <Navbar.Brand as={Link} to="/">My App</Navbar.Brand> {/* ✅ Use as={Link} */}
+        {/* Container is not required, but adds padding */}
+        <Navbar.Brand as={Link} to="/">
+          My Shitty App
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {/* toggle button shown on small screens */}
         <Navbar.Collapse id="basic-navbar-nav">
+          {/* collapsible content */}
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link> {/* ✅ Use as={Link} */}
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            {/* me-auto pushes the content to the right.*/}
+            <Nav.Link as={Link} to="/">
+              {/* as={Link} is required to make the Nav.Link a link */}
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              Contact
+            </Nav.Link>
+            <NavDropdown title="More" id="basic-nav-dropdown">
+              {/* NavDropdown is a dropdown menu */}
+              <NavDropdown.Item as={Link} to="/more1">
+                First item
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/more2">
+                Second item
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              {/* Divider is a horizontal line */}
+              <NavDropdown.Item as={Link} to="/more3">
+                separated element
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
+          <Form className="d-flex">
+            {/* d-flex makes the form flex */}
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-light">Go</Button>
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
